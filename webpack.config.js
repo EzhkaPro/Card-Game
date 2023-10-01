d3b2a054d2c62b2b8c165cddec7e646dccc31b50
EzhkaPro <gala-slim@yandex.ru>
2023-10-02 01:25:37 +0300

    3.5.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+import { resolve as _resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import MiniCssExtractPlugin, { loader } from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = {
-    entry: "./index.ts",
-    mode: isProduction ? "production" : "development",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    optimization: {
-        minimizer: ["...", new CssMinimizerPlugin()],
-    },
-
-    devtool: isProduction ? "hidden-source-map" : "source-map",
-
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-        filename: "bundle.js",
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: "assets", to: "assets" }],
-        }),
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        }),
-        new MiniCssExtractPlugin(),
+export const entry = "./index.ts";
+export const mode = isProduction ? "production" : "development";
+export const module = {
+    rules: [
+        {
+            test: /\.ts$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.css$/i,
+            use: [loader, "css-loader"],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+        },
     ],
 };
+export const resolve = {
+    extensions: [".ts", ".js"],
+};
+export const optimization = {
+    minimizer: ["...", new CssMinimizerPlugin()],
+};
+export const devtool = isProduction ? "hidden-source-map" : "source-map";
+export const output = {
+    path: _resolve(__dirname, "dist"),
+    clean: true,
+    filename: "bundle.js",
+};
+export const plugins = [
+    new CopyPlugin({
+        patterns: [{ from: "assets", to: "assets" }],
+    }),
+    new HtmlWebpackPlugin({
+        template: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+];
